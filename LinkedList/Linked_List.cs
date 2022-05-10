cd249cbf92b5ce95dcbc4d50dc6a0e0e35b31b85
debashissatapathy <erdebashissatapathy62@gmail.com>
2022-05-10 16:39:09 +0530

    Simple Linked List

---
+++ LinkedList/Linked_List.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinkedList
+{
+    class AddNode<T>
+    {
+        public T data;
+        public AddNode<T> Next;
+        public AddNode(T data)
+        {
+            this.data = data;
+        }
+    }
+    class Linked_List<T>
+    {
+       
+
+        internal AddNode<T> head;
+        internal void Add(T data)
+        {
+            AddNode<T> nd = new AddNode<T>(data);
+            if(this.head == null)
+            {
+                this.head = nd;
+            }
+            else
+            {
+                AddNode<T> temp = head;
+                while(temp.Next!=null)
+                {
+                    temp = temp.Next;
+                }
+                temp.Next = nd;
+            }
+            Console.WriteLine("{0} inserted into lined list", nd.data);
+        }
+        internal void Display()
+        {
+            AddNode<T> temp = this.head;
+            if(temp == null)
+            {
+                Console.WriteLine("Linked list is empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.Next;
+            }
+            Console.WriteLine("\n");
+        }
+        
+    }
+}
---
+++ LinkedList/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using LinkedList;
+
+
+class Program
+{
+    static void Main(String[] args)
+    {
+        while (true)
+        {
+            Console.WriteLine("Select a number: \n1.Adding Node ");
+            int option = Convert.ToInt32(Console.ReadLine());
+            switch (option)
+            {
+                case 1:
+                    Linked_List<int> list = new Linked_List<int>();
+                    list.Add(56);
+                    list.Add(30);
+                    list.Add(70);
+                    list.Display();
+                    break;
+                default:
+                    Console.WriteLine("Wrong item selected. Try again.");
+                    Console.WriteLine("\n");
+                    break;
+            }
+        }
+    }
+        
+}
+
+    
+            
+    
